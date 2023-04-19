2f572c704bf8409bb60dab48fc6436e56e47c412
Ajdin Jelin <ajelin@nexussystems.com>
2023-04-19 02:39:51 +0200

    Initial setup

---
+++ Services/Wordify/Reeinvent.Common.Validations/AddSynonymDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Reeinvent.Common.Contracts;
+using System;
+
+namespace Reeinvent.Common.Validations;
+
+public class AddSynonymDtoValidator : AbstractValidator<AddSynonymDto>
+{
+	public AddSynonymDtoValidator()
+	{
+        RuleFor(x => x.Word).NotEmpty().MaximumLength(40);
+        RuleFor(x => x.Synonym).NotEmpty().MaximumLength(40);
+    }
+}
---
+++ Services/Wordify/Reeinvent.Wordify.Core.Services/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Reeinvent.Wordify.Core.Services.Extensions;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ISynonymService, SynonymInMemoryService>();
+
+        return services;
+    }
+}
