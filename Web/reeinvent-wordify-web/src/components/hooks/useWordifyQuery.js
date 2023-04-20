8d72d83a319fba31e3686492dfd04a7302dffcab
Ajdin Jelin <ajelin@nexussystems.com>
2023-04-20 23:58:38 +0200

    Cleanup

--- Web/reeinvent-wordify-web/src/components/hooks/useWordifyQuery.js
+++ Web/reeinvent-wordify-web/src/components/hooks/useWordifyQuery.js
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
 // abstraction of the useQuery, because there is an open issue for the isLoading
-// https://github.com/TanStack/query/discussions/1732
 // didn't want to have to write isLoading && isFetching everywhere
 const useWordifyQuery = ({ tag, parameters, queryFn, enabled, ...options }) => {
   const query = useQuery([tag, ...parameters], () => queryFn(), {
